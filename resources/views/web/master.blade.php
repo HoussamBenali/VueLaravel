aa9e07ff607304b0eaa72b8fc9ce133b93898846
HoussamBenali <houssambenali@gmail.com>
2021-05-14 22:46:46 +0200

    commit

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <link rel="stylesheet" href="{{ secure_asset("css/app.css") }}">
     <title>Módulo admin</title>
 </head>
 
@@ -19,7 +19,7 @@
      
         
     </div>
-    <script src="{{ asset("js/app.js") }}"></script>
+    <script src="{{ secure_asset("js/app.js") }}"></script>
 
 </body>
 
